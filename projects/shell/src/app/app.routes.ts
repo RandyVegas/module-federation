00ce8b04030ac6fa512b47dc2e9378e067f28cef
i.tsarkov <i.tsarkov@tinkoff.ru>
2022-04-15 12:12:04 +0300

    init

---
+++ projects/shell/src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
+export const APP_ROUTES: Routes = [
+    {
+      path: '',
+      component: HomeComponent,
+      pathMatch: 'full'
+    },
+
+    {
+      path: 'flights',
+      loadChildren: () =>
+          loadRemoteModule({
+              type: 'module',
+              remoteEntry: 'http://localhost:3000/remoteEntry.js',
+              exposedModule: './Module'
+          })
+          .then(m => m.FlightsModule)
+    },
+
+    {
+      path: '**',
+      component: NotFoundComponent
+    }
+
+    // DO NOT insert routes after this one.
+    // { path:'**', ...} needs to be the LAST one.
+
+];
+
